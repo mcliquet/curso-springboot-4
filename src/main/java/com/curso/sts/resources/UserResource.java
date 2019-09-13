bc0fb230224f0f566d605c7d8952c0d86a5d634f
Marcus Cliquet <cliquet@gmail.com>
2019-09-13 09:54:13 -0300

    User entity and Resource

---
+++ src/main/java/com/curso/sts/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.curso.sts.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.sts.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	//endpoint de acesso aos usuários
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+				return ResponseEntity.ok().body(u);
+	}
+
+}
